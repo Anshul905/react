aa6d34adf97b1bc84be638a6507cf2e8dfc87cd2
Anshul Neekhara <anshulneekhara@gmail.com>
2024-05-31 11:06:38 +0530

    counter interview question

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -7,7 +7,29 @@ function App() {
 
   let incValue = () => { 
     if( count >= 20 ) alert('it is more than 20') 
-    else  setCount(count+1)
+    else{
+        setCount(count+1)
+
+        // react fibre concept - same task into one batch 
+        // setCount(count+1)
+        // setCount(count+1)
+        // setCount(count+1)
+        // setCount(count+1)
+        
+        // call back concept - next setCount() will be executed after completing the previous one so value will be updated 
+        // setCount() accepts call back function 
+        // if( count <= 16 ){
+        //   setCount( (prevCounter) => (prevCounter+1) ) 
+        //   setCount( (prevCounter) => (prevCounter+1) ) 
+        //   setCount( (prevCounter) => (prevCounter+1) ) 
+        //   setCount( (prevCounter) => (prevCounter+1) ) 
+        // }else{
+        //   alert('max it can be 20.')
+        //   count = 20 ;
+        //   setCount( count ) 
+        // }
+
+      } 
     // console.log(count);
   } 
   let decValue = () => { 
